b6f5c95fdb57e40905e5f33497c678febed367df
Markpo2 <127012533+Markpo2@users.noreply.github.com>
2023-03-14 01:56:42 -0500

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -12,7 +12,6 @@ from plugins.startmsg import Translation
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram import filters
 from pyrogram import Client as Clinton
-from database.access import clinton
 from helper_funcs.display_progress import humanbytes
 from helper_funcs.help_uploadbot import DownLoadFile
 from helper_funcs.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
